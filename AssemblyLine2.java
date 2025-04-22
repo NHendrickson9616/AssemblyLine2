7dda246b7e85a6b5be12231e0c77bee8f993cdc4
nchendri <nchendri@uab.edu>
2025-04-22 11:51:15 -0500

    reordered the creation of items so that none were null

--- AssemblyLine2.java
+++ AssemblyLine2.java
@@ -47,12 +47,12 @@ public class AssemblyLine2{
         prodMap.put("Server Rack", new AssemblyLineProduct("Server Rack", 1, "Crafter MK1", new Pair[]{new Pair(prodMap.get("Aluminum"), 1), new Pair(prodMap.get("Iron"),1)}));
         prodMap.put("Solar Cell", new AssemblyLineProduct("Solar Cell", 1, "Crafter MK1", new Pair[]{new Pair(prodMap.get("Gold"), 1), new Pair(prodMap.get("Diamond Liquid"),1)}));
 
+        prodMap.put("Power Supply", new AssemblyLineProduct("Power Supply", 1, "Crafter MK2", new Pair[]{new Pair(prodMap.get("Circuit"), 1), new Pair(prodMap.get("Diamond"),6), new Pair(prodMap.get("Aluminum Liquid"), 3)}));
+        prodMap.put("Processor", new AssemblyLineProduct("Processor", 1, "Crafter MK2", new Pair[]{new Pair(prodMap.get("Circuit"), 1), new Pair(prodMap.get("Diamond Wire"),3), new Pair(prodMap.get("Gold Liquid"), 3)}));
         prodMap.put("Fan", new AssemblyLineProduct("Fan", 1, "Crafter MK2", new Pair[]{new Pair(prodMap.get("Circuit"), 1), new Pair(prodMap.get("Diamond Gear"),3), new Pair(prodMap.get("Aluminum"), 6)}));
         prodMap.put("Advanced Engine", new AssemblyLineProduct("Advanced Engine", 1, "Crafter MK2", new Pair[]{new Pair(prodMap.get("Circuit"), 5), new Pair(prodMap.get("Engine"),5), new Pair(prodMap.get("Diamond"), 10)}));
         prodMap.put("Computer", new AssemblyLineProduct("Computer", 1, "Crafter MK2", new Pair[]{new Pair(prodMap.get("Fan"), 1), new Pair(prodMap.get("Power Supply"),1), new Pair(prodMap.get("Processor"), 1)}));
         prodMap.put("Laser", new AssemblyLineProduct("Laser", 1, "Crafter MK2", new Pair[]{new Pair(prodMap.get("Battery"), 5), new Pair(prodMap.get("Heater Plate"),5), new Pair(prodMap.get("Iron Liquid"), 5)}));
-        prodMap.put("Power Supply", new AssemblyLineProduct("Power Supply", 1, "Crafter MK2", new Pair[]{new Pair(prodMap.get("Circuit"), 1), new Pair(prodMap.get("Diamond"),6), new Pair(prodMap.get("Aluminum Liquid"), 3)}));
-        prodMap.put("Processor", new AssemblyLineProduct("Processor", 1, "Crafter MK2", new Pair[]{new Pair(prodMap.get("Circuit"), 1), new Pair(prodMap.get("Diamond Wire"),3), new Pair(prodMap.get("Gold Liquid"), 3)}));
         prodMap.put("Solar Panel", new AssemblyLineProduct("Solar Panel", 1, "Crafter MK2", new Pair[]{new Pair(prodMap.get("Circuit"), 1), new Pair(prodMap.get("Solar Cell"),2), new Pair(prodMap.get("Electric Board"), 2)}));
 
         prodMap.put("Copper Plate", new AssemblyLineProduct("Copper Plate", 1, "Hydraulic Press", new Pair[]{new Pair(prodMap.get("Copper"),1)}));
@@ -61,16 +61,16 @@ public class AssemblyLine2{
         prodMap.put("Diamond Plate", new AssemblyLineProduct("Diamond Plate", 1, "Hydraulic Press", new Pair[]{new Pair(prodMap.get("Diamond"),1)}));
         prodMap.put("Iron Plate", new AssemblyLineProduct("Iron Plate", 1, "Hydraulic Press", new Pair[]{new Pair(prodMap.get("Iron"),1)}));
 
+        prodMap.put("Super Computer", new AssemblyLineProduct("Super Computer", 1, "Crafter MK3", new Pair[]{new Pair(prodMap.get("Computer"), 2), new Pair(prodMap.get("Server Rack"),1), new Pair(prodMap.get("Circuit"), 3), new Pair(prodMap.get("Gold Cable"), 3)}));
         prodMap.put("AI Processor", new AssemblyLineProduct("AI Processor", 1, "Crafter MK3", new Pair[]{new Pair(prodMap.get("Circuit"), 10), new Pair(prodMap.get("Super Computer"),1), new Pair(prodMap.get("Copper Plate"), 6), new Pair(prodMap.get("Copper Cable"), 4)}));
-        prodMap.put("AI Robot", new AssemblyLineProduct("AI Robot", 1, "Crafter MK3", new Pair[]{new Pair(prodMap.get("AI Robot Body"), 1), new Pair(prodMap.get("AI Robot Head"),1), new Pair(prodMap.get("Iron Plate"), 10), new Pair(prodMap.get("Diamond Cable"), 5)}));
         prodMap.put("AI Robot Arms", new AssemblyLineProduct("AI Robot Arms", 1, "Crafter MK3", new Pair[]{new Pair(prodMap.get("Laser"), 2), new Pair(prodMap.get("Aluminum Plate"),2), new Pair(prodMap.get("Aluminum Cable"), 3), new Pair(prodMap.get("Iron"), 8)}));
         prodMap.put("AI Robot Body", new AssemblyLineProduct("AI Robot Body", 1, "Crafter MK3", new Pair[]{new Pair(prodMap.get("Electric Engine"), 1), new Pair(prodMap.get("Solar Panel"),2), new Pair(prodMap.get("AI Robot Arms"), 1), new Pair(prodMap.get("Electric Board"), 5)}));
         prodMap.put("AI Robot Head", new AssemblyLineProduct("AI Robot Head", 1, "Crafter MK3", new Pair[]{new Pair(prodMap.get("AI Processor"), 1), new Pair(prodMap.get("Gold Plate"),5), new Pair(prodMap.get("Diamond Cable"),10), new Pair(prodMap.get("Circuit"), 10)}));
         prodMap.put("Electric Engine", new AssemblyLineProduct("Electric Engine", 1, "Crafter MK3", new Pair[]{new Pair(prodMap.get("Battery"), 2), new Pair(prodMap.get("Advanced Engine"),2), new Pair(prodMap.get("Electric Board"), 2), new Pair(prodMap.get("Iron Plate"), 5)}));
         prodMap.put("Explosive", new AssemblyLineProduct("Explosive", 1, "Crafter MK3", new Pair[]{new Pair(prodMap.get("Circuit"), 5), new Pair(prodMap.get("Diamond Wire"),10), new Pair(prodMap.get("Copper Cable"), 10), new Pair(prodMap.get("Heater Plate"), 10)}));
-        prodMap.put("Ignition System", new AssemblyLineProduct("Ignition System", 1, "Crafter MK3", new Pair[]{new Pair(prodMap.get("Trigger"), 2), new Pair(prodMap.get("Explosive"),5), new Pair(prodMap.get("AI Processor"), 2), new Pair(prodMap.get("Battery"), 5)}));
-        prodMap.put("Super Computer", new AssemblyLineProduct("Super Computer", 1, "Crafter MK3", new Pair[]{new Pair(prodMap.get("Computer"), 2), new Pair(prodMap.get("Server Rack"),1), new Pair(prodMap.get("Circuit"), 3), new Pair(prodMap.get("Gold Cable"), 3)}));
         prodMap.put("Trigger", new AssemblyLineProduct("Trigger", 1, "Crafter MK3", new Pair[]{new Pair(prodMap.get("Iron"), 40), new Pair(prodMap.get("Diamond Wire"),10), new Pair(prodMap.get("Circuit"), 5), new Pair(prodMap.get("Electric Board"), 5)}));
+        prodMap.put("Ignition System", new AssemblyLineProduct("Ignition System", 1, "Crafter MK3", new Pair[]{new Pair(prodMap.get("Trigger"), 2), new Pair(prodMap.get("Explosive"),5), new Pair(prodMap.get("AI Processor"), 2), new Pair(prodMap.get("Battery"), 5)}));
+        prodMap.put("AI Robot", new AssemblyLineProduct("AI Robot", 1, "Crafter MK3", new Pair[]{new Pair(prodMap.get("AI Robot Body"), 1), new Pair(prodMap.get("AI Robot Head"),1), new Pair(prodMap.get("Iron Plate"), 10), new Pair(prodMap.get("Diamond Cable"), 5)}));
 
         prodMap.put("Plutonium", new AssemblyLineProduct("Plutonium",4, "Radioactive Starter", null));
         prodMap.put("Uranium", new AssemblyLineProduct("Uranium",4, "Radioactive Starter", null));
@@ -78,14 +78,6 @@ public class AssemblyLine2{
         prodMap.put("Refined Plutonium", new AssemblyLineProduct("Refined Plutonium", 5, "Refinery", new Pair[]{new Pair(prodMap.get("Plutonium"), 1)}));
         prodMap.put("Refined Uranium", new AssemblyLineProduct("Refined Uranium", 5, "Refinery", new Pair[]{new Pair(prodMap.get("Uranium"), 1)}));
 
-        prodMap.put("Nuclear Cell", new AssemblyLineProduct("Nuclear Cell", 5, "Radioactive Crafter MK1", new Pair[]{
-            new Pair(prodMap.get("Uranium Cell"), 1), new Pair(prodMap.get("Plutonium Cell"),1), 
-            new Pair(prodMap.get("Solar Cell"), 3), new Pair(prodMap.get("Electric Board"), 3),
-            new Pair(prodMap.get("Heater Plate"), 3), new Pair(prodMap.get("Refined Plutonium"), 4.2)}));
-        prodMap.put("Nuclear Circuit", new AssemblyLineProduct("Nuclear Circuit", 5, "Radioactive Crafter MK1", new Pair[]{
-            new Pair(prodMap.get("Uranium Circuit"), 1), new Pair(prodMap.get("Plutonium Circuit"),1), 
-            new Pair(prodMap.get("Circuit"), 3), new Pair(prodMap.get("Gold Cable"), 3),
-            new Pair(prodMap.get("Processor"), 3), new Pair(prodMap.get("Refined Plutonium"), 4.2)}));
         prodMap.put("Plutonium Cell", new AssemblyLineProduct("Nuclear Cell", 5, "Radioactive Crafter MK1", new Pair[]{
             new Pair(prodMap.get("Plutonium"), 2), new Pair(prodMap.get("Solar Cell"),2), 
             new Pair(prodMap.get("Diamond Liquid"), 2), new Pair(prodMap.get("Gold Cable"), 2),
@@ -102,12 +94,17 @@ public class AssemblyLine2{
             new Pair(prodMap.get("Uranium"), 4), new Pair(prodMap.get("Circuit"),5), 
             new Pair(prodMap.get("Copper"), 5), new Pair(prodMap.get("Gold Cable"), 1),
             new Pair(prodMap.get("Diamond Wire"), 2), new Pair(prodMap.get("Refined Plutonium"), 4.2)}));
+        prodMap.put("Nuclear Cell", new AssemblyLineProduct("Nuclear Cell", 5, "Radioactive Crafter MK1", new Pair[]{
+            new Pair(prodMap.get("Uranium Cell"), 1), new Pair(prodMap.get("Plutonium Cell"),1), 
+            new Pair(prodMap.get("Solar Cell"), 3), new Pair(prodMap.get("Electric Board"), 3),
+            new Pair(prodMap.get("Heater Plate"), 3), new Pair(prodMap.get("Refined Plutonium"), 4.2)}));
+        prodMap.put("Nuclear Circuit", new AssemblyLineProduct("Nuclear Circuit", 5, "Radioactive Crafter MK1", new Pair[]{
+            new Pair(prodMap.get("Uranium Circuit"), 1), new Pair(prodMap.get("Plutonium Circuit"),1), 
+            new Pair(prodMap.get("Circuit"), 3), new Pair(prodMap.get("Gold Cable"), 3),
+            new Pair(prodMap.get("Processor"), 3), new Pair(prodMap.get("Refined Plutonium"), 4.2)}));
 
-        prodMap.put("AI Robot Bomber", new AssemblyLineProduct("AI Robot Bomber", 5, "Radioactive Crafter MK2", new Pair[]{
-            new Pair(prodMap.get("Atomic Bomb"), 1), new Pair(prodMap.get("AI Robot"),1), 
-            new Pair(prodMap.get("AI Processor"), 2), new Pair(prodMap.get("Nuclear Cell"), 3),
-            new Pair(prodMap.get("Nuclear Processor"), 1), new Pair(prodMap.get("Nuclear Circuit"), 3), 
-            new Pair(prodMap.get("Refined Plutonium"), 4.2)}));
+
+
         prodMap.put("Atomic Bomb", new AssemblyLineProduct("Atomic Bomb", 5, "Radioactive Crafter MK2", new Pair[]{
             new Pair(prodMap.get("Nuclear Processor"), 1), new Pair(prodMap.get("Nuclear Core"),1), 
             new Pair(prodMap.get("Ignition System"), 1), new Pair(prodMap.get("Nuclear Cell"), 1),
@@ -123,7 +120,11 @@ public class AssemblyLine2{
             new Pair(prodMap.get("Plutonium Circuit"), 1), new Pair(prodMap.get("AI Processor"), 1),
             new Pair(prodMap.get("Processor"), 5), new Pair(prodMap.get("Diamond Plate"), 10), 
             new Pair(prodMap.get("Refined Plutonium"), 4.2)}));
-
+        prodMap.put("AI Robot Bomber", new AssemblyLineProduct("AI Robot Bomber", 5, "Radioactive Crafter MK2", new Pair[]{
+            new Pair(prodMap.get("Atomic Bomb"), 1), new Pair(prodMap.get("AI Robot"),1), 
+            new Pair(prodMap.get("AI Processor"), 2), new Pair(prodMap.get("Nuclear Cell"), 3),
+            new Pair(prodMap.get("Nuclear Processor"), 1), new Pair(prodMap.get("Nuclear Circuit"), 3), 
+            new Pair(prodMap.get("Refined Plutonium"), 4.2)}));
     }
 
     public static void main(String[] args) {
